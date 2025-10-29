28e4ed43f35598811f37100a008ac17ecae5e103
Mahendra Dani <danimahendra0904@gmail.com>
2025-10-29 16:39:28 +0530

    feat: init cobra-cli for parsing command line args

--- cmd/gitloom/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"fmt"
-)
-
-func main() {
-	fmt.Println("gitloom: the stupidest version control system")
-}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/MahendraDani/gitloom.git/cmd"
+
+func main() {
+	cmd.Execute()
+}
